3c643d9d1a7ed2a06b1c094882a908d7934d0d51
nqqanhh <anhnq17062004@gmail.com>
2025-10-29 14:42:22 +0700

    fix: update SMTP configuration to use environment variables for user and app password

--- src/services/sendEmail.service.js
+++ src/services/sendEmail.service.js
@@ -3,16 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: "465",
-  secure: true,
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465, // 465 = SSL
+  secure: true, // true cho 465
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.SMTP_USER, // yourgmail@gmail.com
+    pass: process.env.SMTP_APP_PASSWORD, // 16 ký tự App Password
   },
+  pool: true, // optional: connection pool
+  maxConnections: 3,
+  maxMessages: 50,
+  tls: { minVersion: "TLSv1.2" },
+  logger: true, // để xem log
+  debug: true, // để xem log chi tiết SMTP
 });
-
 export default async function sendOTPEmail(recipientEmail, otp) {
   const mailOptions = {
     from: process.env.EMAIL_USER, // Sender address
