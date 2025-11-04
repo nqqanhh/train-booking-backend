58d7840c5bc8556a36b12878c317bb42ea4943e3
nqqanhh <anhnq17062004@gmail.com>
2025-11-04 14:20:44 +0700

    fix: convert vnd to usd

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -158,7 +158,7 @@ const createOrder = async (req, res) => {
         raw: true,
         transaction: t,
       });
-      price = Number(tplSeat?.base_price || 0) / 10000;
+      price = Number(tplSeat?.base_price || 0) / 26000; // Convert VND to USD
       total += price;
       orderItemsPayload.push({
         trip_id: it.trip_id,
